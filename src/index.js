f060fdfb431aae9956c5b2c9d7324831ea17b593
Sam Arbid <36583694+Samk13@users.noreply.github.com>
2019-07-28 20:14:57 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Game from "./Components/TicTacToe"
+
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Tic Tac Toe Game</h1>
+      <div id="container">
+      <Game />
+      </div>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
